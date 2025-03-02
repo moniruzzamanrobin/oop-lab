2c375a297a7d5be350164ec870d83e6f1b1f2587
moniruzzamanrobin <moniruzzamanrobin2@gmail.com>
2025-03-02 23:02:22 +0600

    Create inline-function.cpp

---
+++ day-5/inline-function.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+inline int sum(int num1, int num2)
+{
+    int res = num1 + num2;
+    return res;
+}
+int main()
+{
+    cout<<"summation is: "<<sum(30,50)<<endl;
+}
